e1ebff614423932c44012ff25e4163d7bb55b4dd
Hajime Sato <89377758+hajimesat0@users.noreply.github.com>
2021-12-29 07:12:57 +0900

    dockerによるjupyterlab環境構築スクリプトを追加

---
+++ docker_run.sh
@@ -0,0 +1,2 @@
+docker run -p 8888:8888 -v `pwd`:/home/hostdir -it --rm --name cntn_for_ctrl_eng_study env_for_ctrl_eng_study
+# docker run -p 6080:80 --shm-size=512m -v `pwd`:/home/ubuntu/Desktop/hostdir -it --rm --name ros2_study takasehideki/ros2-desktop-vnc:foxy
